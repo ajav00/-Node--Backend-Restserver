8b9f96fcd30e8a9fd06d57394457b11dcfcf50f0
AJAV <ajalarconvargas@gmail.com>
2021-06-27 15:58:57 -0400

    JWT implementado

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken');
+const Usuario = require('../models/usuario');
+
+const validarJWT = async (req, res, next) => {
+    //Leer el Token
+    const token = req.header('x-token');
+    if (!token) {
+        return res.status(401).json({
+            msg: 'No hay token en la petición'
+        });
+    }
+
+    try {
+        const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        
+        const usuario = await Usuario.findById(uid);
+
+        //Verificar que el usuario este en la BD
+        if(!usuario){
+            return res.status(401).json({
+                msg: 'Token no válido - usuario no existe en DB '
+            })
+        }
+
+        //Verficar que no este inactivo
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Token no válido - usuario con estado: false'
+            })
+        }
+
+        req.usuario = usuario;
+        // req.uid = uid;
+        next();
+
+    } catch (error) {
+
+        return res.status(401).json({
+            msg: 'Token no válido'
+        });
+    }
+
+
+
+}
+
+module.exports = {
+    validarJWT
+}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -6,8 +6,7 @@ const { usuariosGet,
         usuariosDelete,
         usuariosPatch } = require('../controllers/usuarios');
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
-const { validarCampos } = require('../middlewares/validar-campos');
-
+const {validarCampos, validarJWT, esAdminRole, tieneRole} = require('../middlewares')
 const router = Router();
 
 router.get('/', usuariosGet);
@@ -30,6 +29,9 @@ router.put('/:id',[
             ], usuariosPut);
 
 router.delete('/:id', [
+            validarJWT,
+            // esAdminRole,
+            tieneRole('USER_ROLE', 'VENTAS_ROLE'),
             check('id', 'No es un ID válido').isMongoId(),
             check('id').custom( existeUsuarioPorId ),
             validarCampos
